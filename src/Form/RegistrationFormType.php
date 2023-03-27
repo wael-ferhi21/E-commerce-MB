6f58dd7d6a1be61a93f404a3674c5d8b9a05aa83
hamza-007 <hamzaabdellaoui26648999@gmail.com>
2023-03-27 03:51:11 +0100

    wip  :fixing form issue

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -23,9 +23,6 @@ class RegistrationFormType extends AbstractType
             
             ->add('sexe')
             
-            
-            
-            
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
